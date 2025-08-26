497366b379674e257fb22673e5929cc658e13e50
Tran Minh Hien <hientm0978@gmail.com>
2025-08-27 04:51:24 +0700

    ui: Thêm bộ khung giao diện

---
+++ Src/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Src
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 75F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 70); // Adjusted for header
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1264, 611); // Adjusted height
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(10, 10);
+            panel1.Margin = new Padding(10);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(296, 591);
+            panel1.TabIndex = 0;
+            panel1.BackColor = Color.FromArgb(245, 245, 245);
+            // 
+            // comboBox2
+            // 
+            comboBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(15, 375);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(220, 23);
+            comboBox2.TabIndex = 3;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            dateTimePicker1.Location = new Point(15, 205);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(220, 23);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(15, 140);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(220, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(15, 75);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Tên phim...";
+            textBox1.Size = new Size(220, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(flowLayoutPanel1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(326, 10);
+            panel2.Margin = new Padding(10);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(928, 591);
+            panel2.TabIndex = 1;
+            panel2.BackColor = Color.White;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.LeftToRight;
+            flowLayoutPanel1.Location = new Point(0, 50); // Space for content header
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(20);
+            flowLayoutPanel1.Size = new Size(928, 541);
+            flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.WrapContents = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form2";
+            Text = "CinePlex - Ứng dụng đặt vé xem phim";
+            MinimumSize = new Size(800, 600);
+            StartPosition = FormStartPosition.CenterScreen;
+            WindowState = FormWindowState.Maximized;
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        internal TableLayoutPanel tableLayoutPanel1;
+        internal Panel panel1;
+        internal TextBox textBox1;
+        internal ComboBox comboBox2;
+        internal DateTimePicker dateTimePicker1;
+        internal ComboBox comboBox1;
+        internal Panel panel2;
+        internal FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ Src/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Src
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
